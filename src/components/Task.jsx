4e99466652ebc792ef89330dcbc4c9ebc2ecc875
gabrieladutra <gabriela_ddutra@outlook.com>
2023-06-21 21:36:17 -0300

    Corrigido bug do state

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import './Task.css'
 const Task = ({task}) => {
-  return(<div className="task-container">
-  <task className="title"></task></div>)
+  return(<div className="task-container">{task.title}</div>)
   
   
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import Task from "./Task.jsx";
 const Tasks = ({tasks}) => {
   return(<>
-    {tasks.map(task => <Task task={task}/>)} </>)
+    {tasks.map((task) => (<Task task={task}/>))} </>)
 };
 export default Tasks;
